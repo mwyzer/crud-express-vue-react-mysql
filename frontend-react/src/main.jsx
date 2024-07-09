d88f0c9d8ea1c446d11d3f939ec0dba9fba4f9ff
mwyzer <muhammad.wyzer@gmail.com>
2024-07-09 11:53:02 +0700

    install react

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//import BrowserRouter dari react router
+import { BrowserRouter } from "react-router-dom";
+
+//import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
+);
